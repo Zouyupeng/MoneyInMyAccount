9d0e733c4eda92e3452b8a12aabfdaaf3733392c
Oceanknight <2827280689zou@gmail.com>
2023-03-07 16:05:06 +0800

    添加依赖，构建第一个Compose页面

---
+++ app/src/main/java/com/oceanknight/moneyinmyaccount/MimaApp.kt
@@ -0,0 +1,9 @@
+package com.oceanknight.moneyinmyaccount
+
+import android.app.Application
+
+class MimaApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/com/oceanknight/moneyinmyaccount/ui/page/HomeEntry.kt
@@ -0,0 +1,9 @@
+package com.oceanknight.moneyinmyaccount.ui.page
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeEntry(name: String) {
+    Text("Hello $name")
+}
