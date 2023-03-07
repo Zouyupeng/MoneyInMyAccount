95d5cf1a73909b8064d7014e9d1497e49987d3a6
Oceanknight <2827280689zou@gmail.com>
2023-03-07 17:28:32 +0800

    没做对的加一按钮

--- app/src/main/java/com/oceanknight/moneyinmyaccount/MainActivity.kt
+++ app/src/main/java/com/oceanknight/moneyinmyaccount/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.oceanknight.moneyinmyaccount
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.oceanknight.moneyinmyaccount.ui.page.HomeEntry
 
 class MainActivity : ComponentActivity() {
 
+    var num = 1;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent { 
-            HomeEntry(name = "Oceanknight")
+            HomeEntry(num) {
+                Log.d("Num : ", num.toString())
+                ++num;
+            }
         }
-
     }
 }
--- app/src/main/java/com/oceanknight/moneyinmyaccount/ui/page/HomeEntry.kt
+++ app/src/main/java/com/oceanknight/moneyinmyaccount/ui/page/HomeEntry.kt
@@ -1,9 +1,14 @@
 package com.oceanknight.moneyinmyaccount.ui.page
 
+import android.util.Log
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
-fun HomeEntry(name: String) {
-    Text("Hello $name")
+fun HomeEntry(clicks: Int, onClick: () -> Unit) {
+    Button(onClick = onClick) {
+        Log.d("Button : " , clicks.toString())
+        Text("$clicks")
+    }
 }
